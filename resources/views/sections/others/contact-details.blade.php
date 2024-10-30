41dff49fe897ba187b1dd262310b71e2a57c0e75
Arham1243 <arham404khan@gmail.com>
2024-10-30 22:13:15 +0500

    remove contact sec

--- resources/views/sections/others/contact-details.blade.php
+++ resources/views/sections/others/contact-details.blade.php
@@ -1,33 +1,3 @@
-{{-- <div class=tripviz-contact>
-    <div class=container>
-        <div class=tripviz-contact__main>
-            <div class=tripviz-contact__details>
-                <div class=fi1>Tripviz Contact<div class=Questions>Got Questions ?<b> Call us 24/7</b></div>
-                </div>
-                <div class=fi2> <i class="bx bx-phone"></i> +90 552 001 08 40
-                </div>
-                <div class=fi2> <i class="bx bx-envelope"></i> contact@tripviz.com
-                </div>
-                <div class=fi4>
-                    <div class=fi4-1>Social Media<div class=temiz></div><span class=thin>Follow Me <img alt=""
-                                class="darrow2 imgFluid lazy" data-src={{ asset('assets/images/darrow.webp') }}
-                                width=37.19 height=7.56></span></div>
-                    <div class=fi4-2>
-                        <ul>
-                            <li><a target=_blank aria-label="facebook" href=javascript:void(0)><i
-                                        class="bx bxl-facebook-circle"></i></a></li>
-                            <li><a target=_blank aria-label="instagram" href=javascript:void(0)><i
-                                        class="bx bxl-instagram"></i></a></li>
-                            <li><a target=_blank aria-label="twitter" href=javascript:void(0)><i
-                                        class="bx bxl-twitter"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-
 <div class="contact-sec">
     <div class="container">
         <div class="contact-sec__main">
