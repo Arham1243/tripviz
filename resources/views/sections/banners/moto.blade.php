1174bc6876732e8d93c59574f8a1054d701bf9ca
Arham1243 <arham404khan@gmail.com>
2024-12-03 04:40:32 +0500

    fix:  fix enable option for moto

--- resources/views/sections/banners/moto.blade.php
+++ resources/views/sections/banners/moto.blade.php
@@ -1,19 +1,21 @@
-@push('css')
-    <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet">
-@endpush
+@if (isset($content->is_moto_enabled))
+    @push('css')
+        <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet">
+    @endpush
 
-<div class="banner-moto">
-    @if (isset($content->moto_title->title[0]))
-        <div class="banner-moto-title"
-            style="color: {{ isset($content->moto_title->text_color[0]) ? $content->moto_title->text_color[0] : '' }};">
-            {{ $content->moto_title->title[0] }}
-        </div>
-    @endif
+    <div class="banner-moto">
+        @if (isset($content->moto_title->title[0]))
+            <div class="banner-moto-title"
+                style="color: {{ isset($content->moto_title->text_color[0]) ? $content->moto_title->text_color[0] : '' }};">
+                {{ $content->moto_title->title[0] }}
+            </div>
+        @endif
 
-    @if (isset($content->moto_title->title[1]))
-        <div class="banner-moto-title"
-            style="color: {{ isset($content->moto_title->text_color[1]) ? $content->moto_title->text_color[1] : '' }};">
-            {{ $content->moto_title->title[1] }}
-        </div>
-    @endif
-</div>
+        @if (isset($content->moto_title->title[1]))
+            <div class="banner-moto-title"
+                style="color: {{ isset($content->moto_title->text_color[1]) ? $content->moto_title->text_color[1] : '' }};">
+                {{ $content->moto_title->title[1] }}
+            </div>
+        @endif
+    </div>
+@endif
