55fe83f5c8168bc6b7f6b63a235a160650ba92cc
Arham1243 <arham404khan@gmail.com>
2024-12-10 00:44:31 +0500

    change header styling

--- resources/views/admin/dash_layouts/header.blade.php
+++ resources/views/admin/dash_layouts/header.blade.php
@@ -1,20 +1,23 @@
-<div class="dashboard-header">
-    <div class="row">
+<div class="dashboard-header-wrapper">
+    <div class="row g-0">
         <div class="col-md-9">
-            <div class="row justify-content-between">
-                <div class="col-md-4">
-                    <h2>Dashboard</h2>
-                </div>
-                <div class="col-md-5">
-                    <form class="input-search">
-                        <button class="search-icon"><i class='bx bx-search'></i></button>
-                        <input type="text" placeholder="search" name="search">
-                    </form>
+            <div class="dashboard-header">
+                <div class="row justify-content-between">
+                    <div class="col-md-4">
+                        <h2>Dashboard</h2>
+                    </div>
+                    <div class="col-md-5">
+                        <form class="input-search">
+                            <button class="search-icon"><i class='bx bx-search'></i></button>
+                            <input type="text" placeholder="search" name="search">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="col-md-3">
-            <div class="wrapper">
+            <div class="dashboard-header  d-flex justify-content-between pe-2"
+                style="  padding-left: 0 !important;  padding-right: 0.5rem !important; ">
                 <div class="notifi-icon">
                     <i class='bx bxs-bell-ring bx-tada'></i>
                     <div class="notification-count">5</div>
