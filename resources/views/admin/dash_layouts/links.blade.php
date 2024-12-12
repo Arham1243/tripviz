87c8b32c9ec46154298b074bee62051b383397fb
Arham1243 <arham404khan@gmail.com>
2024-12-12 06:43:23 +0500

    add versioning in css

--- resources/views/admin/dash_layouts/links.blade.php
+++ resources/views/admin/dash_layouts/links.blade.php
@@ -1,13 +1,13 @@
 <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 <script defer src="{{ asset('admin/assets/js/alpine.min.js') }}"></script>
-<link href="{{ asset('admin/assets/css/dataTables.min.css') }}" rel="stylesheet">
-<link href="{{ asset('admin/assets/css/select2.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('admin/assets/css/dataTables.min.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('admin/assets/css/select2.min.css?v=' . time()) }}" rel="stylesheet" />
 {{-- <script src="https://cdn.tiny.cloud/1/02gbhrppwyip1quopfi405chacfu5fcsrwsp5xp6fahd69xd/tinymce/7/tinymce.min.js"
     referrerpolicy="origin"></script> --}}
 <script src="https://cdn.ckeditor.com/ckeditor5/39.0.1/classic/ckeditor.js"></script>
-<link href="{{ asset('admin/toast/css/jquery.toast.css') }}" rel="stylesheet">
-<link href="{{ asset('admin/assets/css/bootstrap.min.css') }}" rel="stylesheet">
-<link href="{{ asset('admin/assets/css/fancybox.min.css') }}" rel="stylesheet">
-<link href="{{ asset('admin/assets/fonts/boxicons/boxicons.css') }}" rel="stylesheet">
-<link href="{{ asset('admin/assets/css/style.css') }}" rel="stylesheet">
-<link href="{{ asset('admin/assets/css/responsive.css') }}" rel="stylesheet">
+<link href="{{ asset('admin/toast/css/jquery.toast.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('admin/assets/css/bootstrap.min.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('admin/assets/css/fancybox.min.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('admin/assets/fonts/boxicons/boxicons.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('admin/assets/css/style.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('admin/assets/css/responsive.css?v=' . time()) }}" rel="stylesheet">
--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -1,9 +1,7 @@
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <link href="{{ asset('assets/css/all.min.css') }}" rel="stylesheet">
-    <link rel="preload" href="{{ asset('assets/fonts/boxicons/boxicons.woff2') }}" as="font" type="font/woff2"
-        crossorigin="anonymous">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet">
-
-
-    <link href="{{ asset('admin/login/assets/css/jquery.toast.css') }}" rel="stylesheet">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+<link href="{{ asset('assets/css/all.min.css?v=' . time()) }}" rel="stylesheet">
+<link rel="preload" href="{{ asset('assets/fonts/boxicons/boxicons.woff2') }}" as="font" type="font/woff2"
+    crossorigin="anonymous">
+<link href="{{ asset('assets/css/style.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('assets/css/responsive.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('admin/login/assets/css/jquery.toast.css?v=' . time()) }}" rel="stylesheet">
